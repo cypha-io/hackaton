ab37c9bff760c4933d324efcc09263657d141af7
Paywinful <fiifipayin@gmail.com>
2025-04-21 10:22:23 +0000

    registration removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <ScrollUp />
       <Hero />
       <Features />
-      <CallToAction />
+      {/* <CallToAction /> */}
       <Mentors />
       <Faq />
       <Materials />
