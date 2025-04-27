e223d3c140ea5add793e2c99e379652d230a6ce2
Paywinful <fiifipayin@gmail.com>
2025-04-27 22:07:54 +0000

    Changed to access dataset

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -112,7 +112,7 @@ const About = () => {
                   onClick={() => setShowPopup(true)}
                   className="inline-flex items-center justify-center rounded-md bg-primary px-6 py-2 text-center text-sm font-medium text-white duration-300 hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary"
                 >
-                  Enter Team Details
+                  Access Dataset
                 </button>
               )}
               {isAuthorized && (
